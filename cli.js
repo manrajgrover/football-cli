3f5384667304a07854e684dd35a6546b93667d30
Manraj Singh <manrajsinghgrover@gmail.com>
2016-08-26 19:24:41 +0530

    Config section added | league ids updated

--- cli.js
+++ cli.js
@@ -4,7 +4,7 @@
 * @Author: Manraj Singh
 * @Date:   2016-08-24 12:21:30
 * @Last Modified by:   Manraj Singh
-* @Last Modified time: 2016-08-26 18:22:27
+* @Last Modified time: 2016-08-26 19:14:21
 */
 
 'use strict';
@@ -25,7 +25,28 @@ const headers = {
 
 const argv = yargs
   .usage('$0 <command>')
-  .command('run', 'Runs', (yargs) => {
+  .command('scores', 'Get scores of past and live fixtures', (yargs) => {
+
+  })
+  .command('scores', 'Get scores of past and live fixtures', (yargs) => {
+
+  })
+  .command('scores', 'Get scores of past and live fixtures', (yargs) => {
+
+  })
+  .command('config', 'Change configuration and defaults', (yargs) => {
+    const questions = [{
+      type: 'input',
+      name: 'API_KEY',
+      message: 'Enter API KEY <leave blank incase unchanged>'
+    }];
+    inquirer.prompt(questions).then((answers) => {
+      var obj = config;
+      if (answers.API_KEY !== ''){
+        obj.API_KEY = answers.API_KEY;
+      }
+      fs.writeFileSync(__dirname+'/config.json', JSON.stringify(obj, null, 2), 'utf8');
+    });
   })
   .help('h')
   .alias('h', 'help')
