82194689622e25a791d33d870bbacc9156c0491f
Manraj Singh <manrajsinghgrover@gmail.com>
2016-08-27 02:25:17 +0530

    Revamped code

--- cli.js
+++ cli.js
@@ -4,7 +4,7 @@
 * @Author: Manraj Singh
 * @Date:   2016-08-24 12:21:30
 * @Last Modified by:   Manraj Singh
-* @Last Modified time: 2016-08-27 02:02:41
+* @Last Modified time: 2016-08-27 02:24:52
 */
 
 'use strict';
@@ -21,120 +21,161 @@ const config = require('./config');
 const league_ids = require('./league_ids');
 
 const API_URL = 'http://api.football-data.org/v1/';
+
 const headers = {
   'X-Auth-Token': config.API_KEY
 };
 
 const getURL = (endPoint) => {
-  return API_URL+endPoint;
+  return API_URL + endPoint;
+}
+
+const standings = (err, res, body) => {
+  if(err){
+    throw new Error("Sorry, an error occured");
+  }
+  else{
+    let data = JSON.parse(body), table;
+
+    if(data["standing"] !== undefined){
+
+      let standing = data["standing"];
+
+      table = new Table({
+        head: ['Rank', 'Team', 'Played', 'Goal Diff', 'Points'],
+        colWidths: [ 7, 20, 10, 15, 10]
+      });
+
+      for(let i = 0; i < standing.length; i++) {
+        let team = standing[i];
+        table.push([ team.position, team.teamName, team.playedGames, team.goalDifference, team.points]);
+      }
+
+      console.log(table.toString());
+
+    }
+    else{
+
+      let standings = data["standings"];
+
+      for(let groupCode in standings) {
+
+        console.log(groupCode);
+        let group = standings[groupCode];
+
+        table = new Table({
+          head: ['Rank', 'Team', 'Played', 'Goal Diff', 'Points'],
+          colWidths: [ 7, 20, 10, 15, 10]
+        });
+
+        for(let i = 0; i < group.length; i++){
+          let team = group[i];
+          table.push([ team.rank, team.team, team.playedGames, team.goalDifference, team.points]);
+        }
+        
+        console.log(table.toString());
+      }
+    }
+  }
+}
+
+const refresh = (err, res, body) => {
+
+  if(err){
+
+    console.log("Sorry, an error occured");
+
+  }
+  else {
+
+    let data = JSON.parse(body);
+    let newLeagueIDs = {};
+
+    for(let i = 0; i < data.length; i++) {
+
+      let comp = data[i];
+
+      newLeagueIDs[comp.league] = {
+        "id": comp.id,
+        "caption": comp.caption
+      };
+
+    }
+
+    fs.writeFileSync(__dirname+'/league_ids.json', JSON.stringify(newLeagueIDs, null, 2), 'utf8');
+  }
 }
 
 const argv = yargs
   .usage('$0 <command>')
   .command('scores', 'Get scores of past and live fixtures', (yargs) => {
-    var argv = yargs
+
+    const argv = yargs
       .usage('Usage: $0 scores [options]')
       .alias('l', 'live').describe('l', 'Live scores')
       .example('sudo $0 scores -l')
       .argv;
+
   })
   .command('standings', 'Get standings of particular league', (yargs) => {
-    var argv = yargs
+
+    const argv = yargs
       .usage('Usage: $0 standings [options]')
       .demand('l')
       .alias('l', 'league').describe('l', 'League to be searched')
       .example('sudo $0 standings -l')
       .argv;
-    let id = league_ids[argv.l].id;
-    console.log(id);
-    console.log(getURL(`competitions/${id}/leagueTable`));
-    request({ "url": getURL(`competitions/${id}/leagueTable`), "headers": headers }, (err, res, body) => {
-      if(err){
-        console.log("Sorry, an error occured");
-      }
-      else{
-        var data = JSON.parse(body), table;
 
-        if(data["standing"] !== undefined){
-          let standing = data["standing"];
-          table = new Table({
-            head: ['Rank', 'Team', 'Played', 'Goal Diff', 'Points'],
-            colWidths: [ 7, 20, 10, 15, 10]
-          });
-          for(let i = 0; i < standing.length; i++){
-            let team = standing[i];
-            table.push([ team.position, team.teamName, team.playedGames, team.goalDifference, team.points]);
-          }
-          console.log(table.toString());
-        }
-        else{
-          let standings = data["standings"];
-          for(let table in standings){
-            console.log(table);
-            let group = standings[table];
-            table = new Table({
-              head: ['Rank', 'Team', 'Played', 'Goal Diff', 'Points'],
-              colWidths: [ 7, 20, 10, 15, 10]
-            });
-            for(let i = 0; i < group.length; i++){
-              let team = group[i];
-              table.push([ team.rank, team.team, team.playedGames, team.goalDifference, team.points]);
-            }
-            console.log(table.toString());
-          }
-        }
-      }
-    });
+    let id = league_ids[argv.l].id;
+
+    request({ "url": getURL(`competitions/${id}/leagueTable`), "headers": headers }, standings);
+
   })
   .command('list', 'List of codes of various competitions', (yargs) => {
-    var argv = yargs
+
+    const argv = yargs
       .usage('Usage: sudo $0 list [options]')
       .alias('r', 'refresh').describe('r', 'Refresh league ids').boolean('r')
       .example('sudo $0 config -r')
       .argv;
+
     if (argv.r){
-      request({ "url": getURL("competitions"), "headers": headers }, (err, res, body) => {
-        if(err){
-          console.log("Sorry, an error occured");
-        }
-        else{
-          var data = JSON.parse(body);
-          let newLeagueIDs = {};
-          for(let i=0;i<data.length;i++){
-            let comp = data[i];
-            newLeagueIDs[comp.league] = {
-              "id": comp.id,
-              "caption": comp.caption
-            };
-          }
-          fs.writeFileSync(__dirname+'/league_ids.json', JSON.stringify(newLeagueIDs, null, 2), 'utf8');
-        }
-      });
+
+      request({ "url": getURL("competitions"), "headers": headers }, refresh);
+
     }
     else{
-      var table = new Table({
+
+      let table = new Table({
         head: ['League', 'League Code'],
         colWidths: [ 40, 20]
       });
+
       for(let league in league_ids){
         table.push([ league_ids[league].caption, league]);
       }
+
       console.log(table.toString());
     }
   })
   .command('config', 'Change configuration and defaults', (yargs) => {
+
     const questions = [{
       type: 'input',
       name: 'API_KEY',
       message: 'Enter API KEY <leave blank incase unchanged>'
     }];
+
     inquirer.prompt(questions).then((answers) => {
-      var obj = config;
+      let obj = config;
+
       if (answers.API_KEY !== ''){
         obj.API_KEY = answers.API_KEY;
       }
+
       fs.writeFileSync(__dirname+'/config.json', JSON.stringify(obj, null, 2), 'utf8');
     });
+
   })
   .help('h')
   .alias('h', 'help')
