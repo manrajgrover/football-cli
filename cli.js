57b74de45c1b49c8173c486271e1e6d27ce8a3fd
Manraj Singh <manrajsinghgrover@gmail.com>
2016-08-26 20:48:48 +0530

    Editor Config added

--- cli.js
+++ cli.js
@@ -4,7 +4,7 @@
 * @Author: Manraj Singh
 * @Date:   2016-08-24 12:21:30
 * @Last Modified by:   Manraj Singh
-* @Last Modified time: 2016-08-26 20:25:11
+* @Last Modified time: 2016-08-26 20:48:21
 */
 
 'use strict';
