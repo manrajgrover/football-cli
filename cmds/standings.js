983b164087583b018d4fa25f39306dfadbcee222
Arman Sujoyan <asujoyan@gmail.com>
2018-06-30 18:26:49 +0400

    Clean the sctipts for commands.

--- cmds/standings.js
+++ cmds/standings.js
@@ -6,50 +6,45 @@ const config = require('../config');
 const helpers = require('../helpers');
 const leagueIds = require('../leagueIds');
 
-const fixturesHelper = helpers.fixturesHelper;
+const updateMessage = helpers.updateMessage;
+const standingsHelper = helpers.standingsHelper;
 
 const footballRequest = request.defaults({
-    baseUrl: URLS.API_URL,
-    headers: {
-        'X-Auth-Token': config.API_KEY,
-    },
+  baseUrl: URLS.API_URL,
+  headers: {
+    'X-Auth-Token': config.API_KEY,
+  },
 });
 
 exports.command = 'standings';
 exports.desc = 'Get standings of particular league';
-exports.builder = (yargs) => {
-    return yargs
-        .usage('Usage: $0 standings [options]')
-        .alias('l', 'league')
-            .describe('l', 'League to be searched')
-            .demand('l')
-        .example('$0 standings -l PL')
-        .argv;
+exports.builder = function builder(yargs) {
+  return yargs
+    .usage('Usage: $0 standings [options]')
+    .alias('l', 'league')
+      .describe('l', 'League to be searched')
+      .demand('l')
+    .example('$0 standings -l PL')
+    .argv;
 };
-exports.handler = (yargs) => {
-    /**
-     * Get all the options set for `standings` command
-     */
-    const standings = yargs;
-
-    const spinner = ora('Fetching data').start();
-
-    const league = standings.l;
-
-    if (leagueIds[league] === undefined) {
-      spinner.stop();
-      updateMessage('LEAGUE_ERR');
+exports.handler = function handler(yargs) {
+  /**
+   * Get all the options set for `standings` command
+   */
+  const standings = yargs;
+  const spinner = ora('Fetching data').start();
+  const league = standings.l;
+  if (leagueIds[league] === undefined) {
+    spinner.stop();
+    updateMessage('LEAGUE_ERR');
+  }
+  const id = leagueIds[league].id;
+  footballRequest(`competitions/${id}/leagueTable`, (err, res, body) => {
+    spinner.stop();
+    if (err) {
+      updateMessage('REQ_ERROR');
+    } else {
+      standingsHelper(body);
     }
-
-    const id = leagueIds[league].id;
-
-    footballRequest(`competitions/${id}/leagueTable`, (err, res, body) => {
-      spinner.stop();
-
-      if (err) {
-        updateMessage('REQ_ERROR');
-      } else {
-        standingsHelper(body);
-      }
-    });
-}
+  });
+};
