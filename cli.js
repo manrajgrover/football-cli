bf070c94d0bc5dd96e54510471d6e7a73401be6c
Arman Sujoyan <asujoyan@gmail.com>
2018-06-30 17:42:05 +0400

    Clean the main cli file.

--- cli.js
+++ cli.js
@@ -3,36 +3,6 @@
 'use strict';
 
 const yargs = require('yargs');
-const fs = require('fs');
-const ora = require('ora');
-const chalk = require('chalk');
-const request = require('request');
-const inquirer = require('inquirer');
-const Table = require('cli-table3');
-const config = require('./config');
-const leagueIds = require('./leagueIds');
-const helpers = require('./helpers');
-const path = require('path');
-const URLS = require('./constants');
-
-const footballRequest = request.defaults({
-  baseUrl: URLS.API_URL,
-  headers: {
-    'X-Auth-Token': config.API_KEY,
-  },
-});
-
-/**
- * Get league ids url
- */
-const LEAGUE_IDS_URL = URLS.LEAGUE_IDS_URL;
-
-/**
- * Get all helpers from `helpers.js`
- */
-const fixturesHelper = helpers.fixturesHelper;
-const standingsHelper = helpers.standings;
-const updateMessage = helpers.updateMessage;
 
 /**
  * Command line interface code for the app
